467ab273868c18dd184604c9f5e542c312698122
Daniela Landeta <65782346+dannilandeta@users.noreply.github.com>
2020-12-23 10:58:33 -0500

    Update Materia.java

--- TallerRefactoring/src/modelos/Materia.java
+++ TallerRefactoring/src/modelos/Materia.java
@@ -1,11 +1,16 @@
 package modelos;
 
+import java.util.List;
+
 public class Materia {
     public String codigo;
     public String nombre;
     public String facultad;
-    public double notaInicial;
-    public double notaFinal;
+    public Nota notaInicial;
+    public Nota notaFinal;
     public double notaTotal;
     
+    public double calcularNotaTotal() {
+    	return (notaInicial.calcularNota()+notaFinal.calcularNota())/2;
+    } 
 }
